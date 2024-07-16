0d2961887cf51f0bf8d2943a1c42a3a8fee3cae2
amarjeetsingh7503 <amarjeetsingh75003@gmail.com>
2024-07-16 12:07:41 +0530

    Update tax display

--- public/js/taxInfo.js
+++ public/js/taxInfo.js
@@ -1,11 +1,19 @@
 let taxSwitch = document.getElementById("flexCheckDefault");
 taxSwitch.addEventListener("click", () => {
-  let taxes = document.getElementsByClassName("tax-value");
-  for (info of taxes) {
+  let beforeTaxes = document.getElementsByClassName("before-tax-value");
+  let afterTaxes = document.getElementsByClassName("after-tax-value");
+  for (info of afterTaxes) {
     if (info.style.display != "inline") {
       info.style.display = "inline";
     } else {
       info.style.display = "none";
+    }
+  }
+  for (info of beforeTaxes) {
+    if (info.style.display != "none") {
+      info.style.display = "none";
+    } else {
+      info.style.display = "inline";
     }
   }
 });
